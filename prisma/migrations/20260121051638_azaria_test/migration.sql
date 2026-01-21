cd1a79d39c0f9d055921ea573a349f78258314b9
azaria <azariaplayer@gmail.com>
2026-01-21 16:29:54 -0600

    Deleted old migration

--- prisma/migrations/20260121051638_azaria_test/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Theme" DROP CONSTRAINT "Theme_ownerId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Theme" ADD CONSTRAINT "Theme_id_fkey" FOREIGN KEY ("id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
