7bf15b31cf3bdda9b31670e2c60f7482899f8cdd
JaylinJones0 <jonesjaylink@gmail.com>
2026-01-22 18:30:03 -0600

    (add) routing for users to server/router/router.ts and updated imports

--- server/routers/router.ts
+++ server/routers/router.ts
@@ -3,29 +3,11 @@ import express from 'express';
 import { prisma } from '../database/prisma.js';
 
 import dashboardRouter from './dashboard.js';
+import userRouter from './user.js'
 
 const router = express.Router();
 
+router.use('/user', userRouter);
 router.use('/dashboard', dashboardRouter);
-
-router.post('/user', async (req, res) => {
-  const { name }: { name: string} = req.body;
-
-  try {
-    await prisma.user.create({
-      data: {
-        name,
-        credentialProvider: "",
-        credentialSubject: 0
-      }
-    });
-
-    console.log("Created user... hopefully");
-    res.sendStatus(201);
-  } catch (error) {
-    console.error("Failed to create user:", error);
-    res.sendStatus(500);
-  }
-});
 
 export default router;
