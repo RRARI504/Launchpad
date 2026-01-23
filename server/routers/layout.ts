f46667b75facbaa9614f8bd1656304da01745d06
RRARI504 <ryansimien@ymail.com>
2026-01-23 02:05:07 -0600

    "Feature: CREATE - POST creates copy of public layout by layoutId"

--- server/routers/layout.ts
+++ server/routers/layout.ts
@@ -6,7 +6,7 @@ import { prisma } from '../database/prisma.js';
 
 const layout = express.Router();
 
-// GET
+//GET (route mounted)
 // layout.get('/', (req, res) => {
 //   res.status(200).send('LAYOUT GET');
 // });
@@ -37,10 +37,40 @@ layout.get('/public', async (req, res) => {
 
 //READ: This route will load one layout by id.
 layout.get('/:layoutId', async (req, res) => {
+  //grab layout id
   //needed to be converted to number
   const layoutId = Number(req.params.layoutId);
   try {
+    //query db to find one layout w/ layoutId
     const layout = await prisma.layout.findUnique({
+      where: { id: layoutId },
+      //include all layout elms
+      include: {
+        layoutElements: {
+          //for layout elms include widget
+          include: { widget: true }
+        }
+      }
+    });
+    //check if layout exist first
+    if(!layoutId){
+      return res.status(404).send('Could find layout');
+    }
+    //return layout
+    res.status(200).send(layout)
+  } catch (error) {
+    res.status(500).send({'Could not load layout:': error})
+  }
+
+//CREATE: This route will copy a public layout
+layout.post('/:layoutId/copy', async (req, res) => {
+  console.log('COPY ROUTE HIT', req.params.layoutId);
+  //needed to be converted to number
+  const layoutId = Number(req.params.layoutId);
+  const userId = 1;
+  try {
+    //query db to find one layout w/ layoutId
+    const sourceLayout = await prisma.layout.findUnique({
       where: { id: layoutId },
       include: {
         layoutElements: {
@@ -49,14 +79,36 @@ layout.get('/:layoutId', async (req, res) => {
       }
     });
 
-    if(!layoutId){
-      return res.status(404).send('Could find layout:');
+    if(!sourceLayout || !sourceLayout.public){
+      console.log(sourceLayout)
+      return res.status(404).send('Layout is not public or does not exist');
     }
-    res.status(200).send(layout)
+    //create private copy of layout
+    const newLayout = await prisma.layout.create({
+      data: {
+        ownerId: userId,
+        public: false,
+        gridSize: sourceLayout.gridSize
+      }
+    });
+    //duplicate layout elements
+    await prisma.layoutElement.createMany({
+      data: sourceLayout.layoutElements.map(el =>({
+        layoutId: newLayout.id,
+        widgetId: el.widgetId,
+        posX: el.posX,
+        posY: el.posY,
+        sizeX: el.sizeX,
+        sizeY: el.sizeY
+      }))
+    })
+    //send new copied layout
+    res.status(200).send(newLayout)
   } catch (error) {
-    res.status(500).send({'Could not load layout:': error})
+    res.status(500).send({'Could not copy layout': error})
   }
 })
+});
 
 
 
