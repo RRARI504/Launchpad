a851d9b7e64c4e7d27801bd7cfcc8c6d0550e21a
JaylinJones0 <jonesjaylink@gmail.com>
2026-01-24 16:01:34 -0600

    (add) BroswerRouter from App.tsx to main.tsx, goal of setting up multipage routing soon, but App.tsx refactor needed to achieve this

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,82 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 // import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import Hub from './Hub'
-import Dashboard from './Dashboard';
-import DashEditor from './DashEditor';
+import Hub from "./Hub";
+import Dashboard from "./Dashboard";
+import DashEditor from "./DashEditor";
 
 function App() {
-  const [userDataMessage, setUserDataMessage] = useState('You have not checked User Data.');
-
+  const [userDataMessage, setUserDataMessage] = useState(
+    "You have not checked User Data.",
+  );
+  const [user, setUser] = useState({
+    id: null,
+    name: "",
+    credentialProvider: "",
+    credentialSubject: null,
+    primaryDashId: null,
+  });
   const activeDash = 1; // hardcoded for now
   // eventually want something like:
   // const [activeDash, setActiveDash] = useState(null)
   // what happens if the user doesn't have any dashboards?
 
   const handleLogOut = () => {
-    axios.post('/logout').then((/* Response */) => {
-      // We don't need to do anything with this yet.
-    }).catch((err) => {
-      console.error("There was a problem while logging out", err);
-    })
-  }
+    axios
+      .post("/logout")
+      .then((/* Response */) => {
+        // We don't need to do anything with this yet.
+      })
+      .catch((err) => {
+        console.error("There was a problem while logging out", err);
+      });
+  };
 
   const getUserData = () => {
-    axios.get('/user').then((res) => {
-      setUserDataMessage(res.data);
-    }).catch((err) => {
-      console.error("There was a problem while getting user data", err);
-    })
-  }
+    axios
+      .get("/user")
+      .then((res) => {
+        setUserDataMessage(res.data);
+      })
+      .catch((err) => {
+        console.error("There was a problem while getting user data", err);
+      });
+  };
 
   return (
     <>
       <h1>Rendering</h1>
       <h1>Sign in</h1>
-      <a className="button google" href="/login/federated/google">Sign in with Google</a>
-      <button className="logout button google" onClick={() => {handleLogOut()}}>Log Out</button>
-      <button className="testGetUserData" onClick={() => {getUserData()}}>Get User Data</button>
+      <a className="button google" href="/login/federated/google">
+        Sign in with Google
+      </a>
+      <button
+        className="logout button google"
+        onClick={() => {
+          handleLogOut();
+        }}
+      >
+        Log Out
+      </button>
+      <button
+        className="testGetUserData"
+        onClick={() => {
+          getUserData();
+        }}
+      >
+        Get User Data
+      </button>
       <p>{userDataMessage}</p>
-      <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Dashboard dashboardId={activeDash}/>} />
-          <Route path='/edit' element={<DashEditor dashboardId={activeDash}/>} />
-          <Route path='/hub' element={<Hub></Hub>}/>
+          <Route path="/" element={<Dashboard dashboardId={activeDash} />} />
+          <Route
+            path="/edit"
+            element={<DashEditor dashboardId={activeDash} />}
+          />
+          <Route path="/hub" element={<Hub />} />
         </Routes>
-      </BrowserRouter>
     </>
   );
 }
